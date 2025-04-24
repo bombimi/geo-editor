4e849a70bd8961d641f3244264e613ea3d31cec5
Martin Slater <mslater@darkstorm.com.au>
2025-04-24 10:19:53 +1000

    chore: delete unused files

--- lib/editor/EditorProvider.ts
+++
@@ -1,7 +0,0 @@
-import { DocumentProvider } from "./DocumentProvider";
-import { Modifier } from "./Modifier";
-
-export abstract class EditorProvider {
-    public abstract get documentProviders(): DocumentProvider[];
-    public abstract get modifiers(): Modifier[];
-}
